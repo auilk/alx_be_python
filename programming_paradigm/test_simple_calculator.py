f1239f6e91eee9249f7bb990ce61d0c88d2e3af7
auilk <abdelghafour.lbaz01@gmail.com>
2025-08-03 22:30:13 +0100

    Finish third task

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,38 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test various addition scenarios."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+        self.assertEqual(self.calc.add(-5, -10), -15)
+        self.assertAlmostEqual(self.calc.add(2.5, 3.2), 5.7)
+
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(10, 3), 7)
+        self.assertEqual(self.calc.subtract(0, 5), -5)
+        self.assertEqual(self.calc.subtract(-4, -6), 2)
+        self.assertAlmostEqual(self.calc.subtract(7.5, 2.5), 5.0)
+
+    def test_multiplication(self):
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
+        self.assertEqual(self.calc.multiply(-3, 3), -9)
+        self.assertAlmostEqual(self.calc.multiply(2.5, 4.0), 10.0)
+
+    def test_division(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertIsNone(self.calc.divide(5, 0))
+        self.assertAlmostEqual(self.calc.divide(7.5, 2.5), 3.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
