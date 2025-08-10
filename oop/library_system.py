da403412e90b6d989e4354b609eff215dc2c4f2b
auilk <abdelghafour.lbaz01@gmail.com>
2025-08-10 17:21:19 +0100

    Finish second task

---
+++ oop/library_system.py
@@ -0,0 +1,33 @@
+from book_class import Book
+
+class EBook(Book):
+    def __init__(self, title, author, year, file_size):
+        super().__init__(title, author, year)
+        self.file_size = file_size
+
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+
+class PrintBook(Book):
+    def __init__(self, title, author, year, page_count):
+        super().__init__(title, author, year)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only Book, EBook, or PrintBook instances can be added.")
+
+    def list_books(self):
+        for book in self.books:
+            print(book)
