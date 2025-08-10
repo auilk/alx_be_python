db8c83ff6310a054fa9dd80091358e94048b578e
auilk <abdelghafour.lbaz01@gmail.com>
2025-08-10 17:32:16 +0100

    Fix output to match task requirement

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,15 +4,15 @@ class Book:
         self.author = author
         self.year = year
 
-    def get_details(self):
-        return f"Book: {self.title} by {self.author}"
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}, published in {self.year}"
 
 class EBook(Book):
     def __init__(self, title, author, year, file_size):
         super().__init__(title, author, year)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -21,7 +21,7 @@ class PrintBook(Book):
         super().__init__(title, author, year)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -37,4 +37,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)
